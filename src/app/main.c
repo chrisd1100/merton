c6a3b4f038d090a614eee2b208a9ff28deacaee5
Christopher Dickson <chrisd1100@gmail.com>
2020-05-01 10:24:09 -0400

    cleanup

--- src/app/main.c
+++ src/app/main.c
@@ -465,11 +465,13 @@ int32_t WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmd
 {
 	hInstance; hPrevInstance; lpCmdLine; nCmdShow;
 
+	/*
 	AllocConsole();
 	AttachConsole(GetCurrentProcessId());
 
 	FILE *f = NULL;
 	freopen_s(&f, "CONOUT$", "w", stdout);
+	*/
 
 	timeBeginPeriod(1);
 
