86de68d19a4e596853eaa405f57509588e8e6174
Christopher Dickson <chrisd1100@gmail.com>
2020-05-05 12:32:38 -0400

    Revert "remove soft scanlines"
    This reverts commit 5c5232f3dc70c7dcb202fef7945fa50e1ad1d90b.

--- src/lib/windows/shaders/ps.hlsl
+++ src/lib/windows/shaders/ps.hlsl
@@ -11,9 +11,5 @@ Texture2D frame: register(t0);
 float4 main(VS_OUTPUT input) : SV_TARGET
 {
 	float4 rgba = frame.Sample(ss, input.texcoord);
-
-	if (fmod(input.texcoord.y, 2.0) > 0.0)
-		rgba.a = rgba.r = rgba.g = rgba.b = 0.0;
-
 	return float4(rgba.b, rgba.g, rgba.r, rgba.a);
 }
