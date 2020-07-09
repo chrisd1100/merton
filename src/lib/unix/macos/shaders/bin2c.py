2d49be59c7bd41ab779d507022789b0639540a59
Christopher Dickson <chrisd1100@gmail.com>
2020-07-09 17:27:07 -0400

    migrate lib to matoya

--- src/lib/unix/macos/shaders/bin2c.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-a = []
-for b in open(sys.argv[1], 'rb').read():
-	a.append('%d' % ord(b))
-
-f = open(sys.argv[2], 'w')
-f.write('static unsigned char %s[] = {%s};' % (sys.argv[3], ', '.join(a)))
